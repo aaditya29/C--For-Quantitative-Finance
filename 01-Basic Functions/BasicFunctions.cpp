b4dbc4659832ef3ace78eb0cfcb37840e9adda98
Aditya Mishra <mishraaaditya29@gmail.com>
2023-07-13 20:08:51 +0530

    .DS_Store Updated

--- 01-Basic Functions/BasicFunctions.cpp
+++ 01-Basic Functions/BasicFunctions.cpp
@@ -5,31 +5,33 @@ using namespace std;
 int main()
 {
     const int monthsInYear = 12;
-    
+
     double principal = 0.0;
-    
-    cout << "Enter the principal amount: "<<endl;
-    
+
+    cout << "Enter the principal amount: " << endl;
+
     cin >> principal;
-    
+
     double interest = 0.0;
-    
-    cout << "Enter the interest rate: "<<endl;
-    
+
+    cout << "Enter the interest rate: " << endl;
+
     cin >> interest;
-    
+
     int yearsOfLoan = 0;
-    
-    cout << "Enter the years of loan: "<<endl;
-    
+
+    cout << "Enter the years of loan: " << endl;
+
     cin >> yearsOfLoan;
-    
-    double monthInterest = interest/monthsInYear;
+
+    double monthInterest = interest / monthsInYear;
     long monthsOfLoan = yearsOfLoan * monthsInYear;
-    
-    cout << principal << " " <<endl<< interest << " " <<endl<<
-    yearsOfLoan << " " <<endl<< monthInterest << " " <<endl<<
-    monthsOfLoan << endl;
-    
+
+    cout << principal << " " << endl
+         << interest << " " << endl
+         << yearsOfLoan << " " << endl
+         << monthInterest << " " << endl
+         << monthsOfLoan << endl;
+
     return 0;
 }
