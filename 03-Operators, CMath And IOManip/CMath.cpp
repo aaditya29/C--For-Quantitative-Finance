04d885ce28ee6d77a9c1d471b905f4a4e603c379
Aditya Mishra <mishraaaditya29@gmail.com>
2023-07-15 18:43:14 +0530

    Module-3 Completed

--- 03-Operators, CMath And IOManip/CMath.cpp
+++ 03-Operators, CMath And IOManip/CMath.cpp
@@ -1,5 +1,7 @@
 // Calculating monthly payment
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -44,11 +46,17 @@ int main()
     double monthInterest = divisor(interest, monthsInYear);
     long monthsOfLoan = multiplier(yearsOfLoan, monthsInYear);
 
-    cout << "The principal is: " << principal << " " << endl
-         << "The interest rate is(in percentage): " << interest << " " << endl
-         << "The number of years for loan is: " << yearsOfLoan << " " << endl
-         << "The months of interest is: " << monthInterest << " " << endl
-         << "The number of months for loan is: " << monthsOfLoan << endl;
+    double payment = 0.0;
+    payment = principal *
+              (monthInterest /
+               (1 - (pow((double)1 + monthInterest,
+                         (double)-monthsOfLoan))));
+
+    cout << "Payment: " << setiosflags(ios::fixed) << setprecision(2) << payment << endl;
+    /*
+    1) setiosflags: The setiosflags function sets the flag bits in mask for a stream. In other words, it performs the equivalent of stream .setf(mask)
+    2)setprecision(): The setprecision() method of iomanip library in C++ is used to set the ios library floating point precision based on the precision specified
+    */
 
     return 0;
 }
