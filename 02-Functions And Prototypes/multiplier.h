6db9570c40bd80199dad534175f5ec3c256af9c2
Aditya Mishra <mishraaaditya29@gmail.com>
2023-07-15 09:11:33 +0530

    multiplier.h added

---
+++ 02-Functions And Prototypes/multiplier.h
@@ -0,0 +1,9 @@
+#include "multiplier.h"
+
+double multiplier(double firstNum, double secondNum)
+{
+    // This function multiplies two numbers
+    // and returns the result.
+
+    return firstNum * secondNum;
+}
