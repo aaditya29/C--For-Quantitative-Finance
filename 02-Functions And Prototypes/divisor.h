dd2debc480d50ec3816d61c13ea5a48734b51a32
Aditya Mishra <mishraaaditya29@gmail.com>
2023-07-15 09:22:03 +0530

    Files Updated

--- 02-Functions And Prototypes/divisor.h
+++
@@ -1,9 +0,0 @@
-#include "divisor.h"
-
-double divisor(double numerator, double denominator)
-{
-    // This function divides two numbers
-    // and returns the result.
-
-    return numerator / denominator;
-}
--- 02-Functions And Prototypes/multiplier.h
+++
@@ -1,9 +0,0 @@
-#include "multiplier.h"
-
-double multiplier(double firstNum, double secondNum)
-{
-    // This function multiplies two numbers
-    // and returns the result.
-
-    return firstNum * secondNum;
-}
